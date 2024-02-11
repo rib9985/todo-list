ce9f73a21f2ae86228d23bcefd04c8842ed3b26b
rib9985 <rib9985@gmail.com>
2024-02-11 17:48:37 -0300

    Create Project module

--- src/modules/Logic.js
+++ src/modules/Logic.js
@@ -1,9 +1,10 @@
 import Todo from "./Todo";
 //
-const myTodos = []
+
+const projects = []
 
 
-// Define a function to parse todo form
+// Parse todo form -> Returns Todo Task Object
 
 function parseTodoForm(){
     const title = document.getElementById('todo-form-title').value
@@ -16,3 +17,35 @@ function parseTodoForm(){
     return todoTaskObject && myTodos.push(todoTaskObject)
 }
 
+// Push Todo to Project -> Returns Updated Project
+function addTodoToProject(todo, project){
+    const currentProject = this.project
+    const todoName = this.todo
+
+    currentProject.key = todoName
+    currentProject.value = todo
+    
+    return currentProject
+}
+
+// Activate Project -> Returns Active Project
+function getActiveProject (name){
+
+    
+    
+
+
+}
+
+// Edit Project -> Return Old Project, with new Name
+
+// Retrieves a Todo by Name -> Returns Todo by Name
+
+// Delete a Todo -> Returns Updated Project, without Todo
+
+// Edit a Todo -> Returns Todo
+
+// Assign Status -> Pushes Todo to Deleted Folder in Trash
+
+// TODO: Temporary storage, research how this works with JSON
+// Store Info
