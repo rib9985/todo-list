899690ca6036d5a4d8bbe9722f2139a4390cd738
rib9985 <rib9985@gmail.com>
2024-02-13 10:21:50 -0300

    Add styles and change Logic

--- src/index.js
+++ src/index.js
@@ -1,15 +1,3 @@
 import Todo from "./modules/Todo";
-
+import UI from "./modules/EventListeners"
 import './style.css';
-
-function returnFlex(){const mainDivSelect = document.getElementById('div-main')
-mainDivSelect.classList.add('divMain')
-}
-
-function addLogo(){
-    const logoDiv = document.getElementById('div-logo')
-    logoDiv.classList.add('logo')
-    return logoDiv
-}
-
-addLogo()
