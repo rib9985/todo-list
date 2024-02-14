623237d5b0c3a0b4d0ef9bf2a6d9a2922fe6ce96
rib9985 <rib9985@gmail.com>
2024-02-14 14:07:25 -0300

    Refactor UI and Logic for proper flow

--- src/modules/Logic.js
+++ src/modules/Logic.js
@@ -5,33 +5,50 @@ import Project from "./Project"
 
 const projects = []
 
+function instantiateDefaultProjects (){
+    const inbox = new Project('Inbox', [])
+    const today = new Project('Today', [])
+    const upcoming = new Project('Upcoming', [])
+    const anytime = new Project('Anytime', [])
+
+    projects.push(inbox, today, upcoming, anytime)
+    return projects
+}
+
 
 // Parse todo form -> Returns Todo Task Object
 
 function parseTodoForm(){
-    const title = document.getElementById('todo-form-title').value
-    const description = document.getElementById('todo-form-description').value
-    const complete = document.getElementById('todo-form-complete').value
-    const dueDate = document.getElementById('todo-form-dueDate').value
-    const priority = document.getElementById('todo-form-priority').value
+    const status = false
+    const title = document.getElementById('form-todo-title').value
+    const description = document.getElementById('form-todo-description').value
+    const notes = document.getElementById('form-todo-notes').value
+    const dueDate = document.getElementById('form-todo-date').value
+    const priority = document.getElementById('form-todo-priority').value
 
-    const todoTaskObject = new Todo(title, description, complete, dueDate,priority)
-    console.log(todoTaskObject) 
+    const todoTaskObject = new Todo(status, title, description, notes, dueDate, priority)
     return todoTaskObject 
 }
 
-// Push Todo to Project -> Returns Updated Project
-function addTodoToProject(todo, project){
-    project.pushTodoToProject(todo)
-    return project
-}
-
 // Activate Project -> Returns Active Project
-function getActiveProject (name){
-    return projects.find(name)
+
+function getActiveProject (project){
+   const result = projects.find(( { name }) => name === project   )
+   return result
 }
 
+console.log (getActiveProject('Inbox'))
 
-export {parseTodoForm, addTodoToProject,getActiveProject, projects}
+function addTodoToProject(todo, currentProject){
+   const targetProject = getActiveProject(currentProject)
+   if (targetProject){
+    targetProject.projectTodos.push(todo)
+   } 
+   else{
+    console.log(`Project ${targetProject} not found`)
+   }
+}
+
+export {parseTodoForm, addTodoToProject,getActiveProject, instantiateDefaultProjects, projects}
 // TODO: Temporary storage, research how this works with JSON
 // Store Info
--- src/modules/UI.js
+++ src/modules/UI.js
@@ -1,30 +1,50 @@
 import * as logic from './Logic' 
-
+import * as Component from './'
 export default class UI{
 
+static elementIds(){
+    
+}
+
 static addEventListeners(){
-    const submitTodoButton = document.getElementById('submit-todo-button')
+    const formSubmitTodo = document.getElementById('form-submit-todo')
     const todoCheckbox = document.getElementById('todo-checkbox')
     const todoPriority = document.getElementById('div-todo-priority-icon')
     const sidebarProject = document.getElementById('sidebar-project')
 
-    submitTodoButton.addEventListener('click', UI.handleFormElements(Event))
+    formSubmitTodo.addEventListener('submit', (event) => {UI.handleTodoForm(event)})
 }
 
 static getActiveProject(){
-    const activeProject = document.getElementById('')
-
+    const activeProject = document.getElementById('div-current-project').innerHTML
+    console.log(`Current Active Project is ${activeProject}`)
+    return activeProject
 }
 
 static changePriorityClass(){
 
 }
 
-
-static handleFormElements(Event){
-    Event.preventDefault()
-    logic.parseTodoForm()
-
+static clearProjectUI(){
+    const todosDiv
 }
+
+
+static handleTodoForm(Event){
+    Event.preventDefault()
+    const currentProject = UI.getActiveProject() 
+    const todo = logic.parseTodoForm()
+
+    logic.addTodoToProject(todo,currentProject)
+}
+
+static intializeUI(){
+    UI.addEventListeners()
+}
+
+
+
+static createTodoComponent(){
+
 
 }
