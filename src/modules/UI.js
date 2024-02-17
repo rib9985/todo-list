63a8b76d6ad69d3579d11e7b7aa5f49f30cfa5e9
rib9985 <rib9985@gmail.com>
2024-02-17 11:29:55 -0300

    Create todo component, remove methods

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -1,15 +1,16 @@
 import * as logic from './Logic' 
-import * as Component from './'
+
+
+
 export default class UI{
 
-static elementIds(){
-    
+static idsDom(){
 }
 
 static addEventListeners(){
     const formSubmitTodo = document.getElementById('form-submit-todo')
     const todoCheckbox = document.getElementById('todo-checkbox')
-    const todoPriority = document.getElementById('div-todo-priority-icon')
+    const todoPriority = document.getElementsByClassName('div-todo-priority')
     const sidebarProject = document.getElementById('sidebar-project')
 
     formSubmitTodo.addEventListener('submit', (event) => {UI.handleTodoForm(event)})
@@ -21,12 +22,11 @@ static getActiveProject(){
     return activeProject
 }
 
-static changePriorityClass(){
-
-}
 
 static clearProjectUI(){
-    const todosDiv
+   const divTodoList = document.getElementById('todo-list')
+   divTodoList.innerHTML = ''
+
 }
 
 
@@ -36,15 +36,43 @@ static handleTodoForm(Event){
     const todo = logic.parseTodoForm()
 
     logic.addTodoToProject(todo,currentProject)
+    UI.createTodoComponent(todo.title, todo.notes, todo.description, todo.dueDate, todo.priority)
 }
 
 static intializeUI(){
-    UI.addEventListeners()
 }
 
 
 
-static createTodoComponent(){
+static createTodoComponent(title, description, notes, dueDate, priority){
+   const divTodoList = document.getElementById('todo-list')
+   const todoComponentHTML = 
+    `<div class="todo"> 
+            <div id="div-todo-component">
+                <input type="checkbox" id="todo-checkbox">
+                <label for="todo-checkbox" id="todo-checkbox-label">
+                <div id="div-todo-title">${title}</div>
+                <div id="div-todo-description">${description}</div>
+                <div id="div-todo-notes">${notes}</div>
+                <div id="div-todo-date">${dueDate}</div>
+        </label>
+                <button id="todo-priority-${priority}" class="material-symbols-outlined button-todo-priority">priority_high</button>
+                <button id="div-todo-edit-icon" class="material-symbols-outlined">edit</button>
+            </div>
+    </div>`
+
+    divTodoList.insertAdjacentHTML('beforeend', todoComponentHTML);
+
+    const todoComponents = document.querySelectorAll('.todo');
+    const lastTodoComponent = todoComponents[todoComponents.length - 1];
+    const buttonTodoPriority = lastTodoComponent.querySelector('.button-todo-priority');
+
+    buttonTodoPriority.addEventListener('click', function() {
+        const newPriorityStatus = logic.togglePriorityStatus(UI.getActiveProject(), title);
+        this.id = `todo-priority-${newPriorityStatus}`; // Update the button's id
+})
 
 
-}
+
+
+}}
