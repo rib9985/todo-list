e7cbbb560e3b2b0dabc598ca2706f5b54619c48f
rib9985 <rib9985@gmail.com>
2024-02-17 11:29:15 -0300

    Add id generator property to todo

--- src/modules/Todo.js
+++ src/modules/Todo.js
@@ -1,3 +1,5 @@
+import idGenerator from "./IdGenerator"
+
 export default class Todo{
 
     // This creates the initial object of the todo
@@ -8,8 +10,20 @@ export default class Todo{
         this.notes =notes
         this.dueDate = dueDate
         this.priority=priority
+        this.id = idGenerator()
     }
 
+    changePriority(){
+        if (this.priority === 'low'){
+            this.priority = 'medium'
+        }
+        else if (this.priority === 'medium'){
+            this.priority = 'high'
+        }
+        else{
+            this.priority = 'low'
+        }
+    }
 
     dateFormatted(){
         if (!this.date){
