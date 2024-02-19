ca6c0720f70ad0997e67f1e879212322332ba595
rib9985 <rib9985@gmail.com>
2024-02-19 11:06:46 -0300

    Refactor random id module

--- src/modules/IdGenerator.js
+++ src/modules/IdGenerator.js
@@ -1,6 +1,8 @@
 
 export default function idGenerator(){
-const id = () => Math.floor(Math.random()*100).toString
-return id
+const id = () => Math.floor(Math.random()*100000).toString()
+return id()
 }
 
+const newId = idGenerator()
+console.log(newId)
